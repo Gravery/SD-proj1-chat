c1193b9e87e16998967d0c5e11565de3466492b0
Guilherme Calça <guilhermecalca@hotmail.com>
2022-07-22 08:30:10 -0300

    Comentarios adicionados

--- publisher.py
+++ publisher.py
@@ -1,14 +1,23 @@
-import zmq, time
+#Programa para rodar um publisher
 
+#Import da biblioteca zmq
+import zmq
+
+#Setup dos dados do publisher
 context = zmq.Context()
 s = context.socket(zmq.PUB)
-s.bind("tcp://*:5555")
+port = input("Escolha uma port para hospedar(Ex: 1111): ")
+s.bind("tcp://*:" + port)
 
+#Criação do loop para envio contínuo de mensagens
 run = 1
-
 while run:
+    #Input das mensagens no formato especificado
     msg = input("Formato da mensagem: [TOPICO] [NOME] [MENSAGEM]: ")
+    
+    #Condição de parada do publisher
     if msg == "000":
         break
-
+    
+    #Envio gerenciado pela biblioteca para todos inscritos
     s.send_string(msg)
--- subscriber.py
+++ subscriber.py
@@ -1,10 +1,11 @@
-from curses import keyname
-import zmq
-import keyboard
-from multiprocessing import Process
+#Programa para rodar um subscriber
 
+#Import da biblioteca zmq
+import zmq
+
+#Função para realizar ações de conexão, inscrição e recebimento de mensagens
 def choose(s):
-    choice = int(input('1 - Conectar a um endereço\n2 - Inscrever em um tópico\n3 - Seguir para recebimento de mensagens'))
+    choice = int(input('1 - Conectar a um endereço\n2 - Inscrever em um tópico\n3 - Seguir para recebimento de mensagens\n'))
     print()
     if choice == 1:
         new_connect(s)
@@ -14,24 +15,30 @@ def choose(s):
         return
     choose(s)
 
-
+#Função para conectar a um novo endereço a partir do IP + port
 def new_connect(s):
         address = input("Insert Addres[IP:PORT]: ")
         s.connect(f"tcp://{address}")
 
+#Função para inscrição em um tópico gerenciado pela bilbioteca zmq
 def new_sub(s):
         subs = input("Digite um tópico para se inscrever: ")
         s.setsockopt_string(zmq.SUBSCRIBE, subs)
 
 
+#Inicio do programa
+
+#Setup inicial com inscrição já realizada no tópico de TESTE
 context = zmq.Context()
 s = context.socket(zmq.SUB)
 s.connect("tcp://localhost:5555")
 s.setsockopt_string(zmq.SUBSCRIBE, "TESTE")
 
+#Chamada da função para realizar as ações
 choose(s)
 run = 1
 
+#Loop infinito para recebimento de mensagens nos tópicos inscritos de todas as conexões
 while run:
     msg = s.recv()
     msg = msg.decode()
